6030825a2fbfe430cc92d193a01b876508ec5831
Coby <cobyrpilpel2@gmail.com>
2021-01-03 21:26:37 -0500

    commit tree 181b0c1f206e50e66186dfd1b8883283e09f1130

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,28 @@
 
+
+class Appointment
+attr_accessor :date, :doctor, :patient
+@@all = []
+
+def initialize(date,patient,doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+    save
+end
+
+def self.all
+    @@all
+end
+def save
+    Appointment.all << self
+end
+
+end 
+
+
+
+
+
+
